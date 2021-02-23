dd6bd4a6545bb98064f7c7fb3ff5a3d394690c91
Seagyn Davis <seagyn@gmail.com>
2021-02-23 23:40:37 +0200

    add tracking and fix stuff

---
+++ src/pages/_document.jsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en-GB">
+        <Head>
+          <script
+            src="https://lamprey.seagyndavis.com/script.js"
+            data-site="KYTUNHDH"
+            defer
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
