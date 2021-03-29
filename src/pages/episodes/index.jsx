2d2105550a0bd91aa868a7463295c92fcf741a31
Seagyn Davis <seagyn@gmail.com>
2021-03-29 23:35:58 +0200

    go go go

--- src/pages/episodes/index.jsx
+++ src/pages/episodes/index.jsx
@@ -1,10 +1,9 @@
 import { episodes } from "utils/data";
 
 const Episodes = () => {
-  console.log(episodes);
   return episodes.map(episode => {
-    console.log(episode.module.default);
-    return<div key={episode.link}>{episode.module.default}</div>
+    console.log(episode);
+    return <div key={episode.link}>{episode.module.default.components}</div>
   });
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       </Head>
       <p className="text-white text-xl mb-10">Now Now is a podcast by two friends, Andrew Lima and Seagyn Davis, who like talking about tech, the internet, startups, and investing. We share our life experiences and what we're currently doing to motivate you to do the same.</p>
       <h2 className="text-white text-3xl font-bold mb-8">Latest Podcast</h2>
-      <iframe src="https://anchor.fm/nownow/embed/episodes/Ep-0---Prologue-eqtols" height="102px" className="w-full" frameborder="0" scrolling="no"></iframe>
+      <iframe src="https://anchor.fm/nownow/embed/episodes/Ep-1---Starting-a-side-hustle-etqpiv" height="102px" className="w-full" frameborder="0" scrolling="no"></iframe>
     </>
   )
 }
